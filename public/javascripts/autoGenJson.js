9f979985803f96e87358870d16d8cb2ccedd7130
luyuannie <nie.luyuan@gmail.com>
2016-06-30 23:21:38 -0700

    second commit

--- public/javascripts/autoGenJson.js
+++ public/javascripts/autoGenJson.js
@@ -3,7 +3,19 @@
  */
 (function(){
     var app = angular.module("autoGenJson",[]);
-    app.controller("autoGenController",[function(){
-
+    app.controller("autoGenController",['$scope',function($scope){
+        $scope.createJson= function(attr,str){
+            var spilt = str.split('\n');
+            console.log(spilt);
+            var obj =[];
+            for(var i=0;i<spilt.length;i++){
+                var temp={};
+                temp["id"]=i;
+                temp[attr]=spilt[i];
+                obj.push(temp);
+            }
+            $scope.displayObj=JSON.stringify(obj);
+            console.log(obj);
+        }
     }])
 })();
